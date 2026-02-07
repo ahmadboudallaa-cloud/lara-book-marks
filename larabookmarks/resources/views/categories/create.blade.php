a27d56747818ab352e473ba0fac3f1a1cffeaa06
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-02-07 01:07:50 +0100

    diagrammme de clésee

--- larabookmarks/resources/views/categories/create.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <h1 class="text-xl font-bold mb-4">Ajouter une catégorie</h1>
-
-    <form method="POST" action="{{ route('categories.store') }}" class="space-y-2">
-        @csrf
-        <input type="text" name="name" placeholder="Nom de la catégorie" class="border p-2 w-full" required>
-        <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Ajouter</button>
-    </form>
-</x-app-layout>
