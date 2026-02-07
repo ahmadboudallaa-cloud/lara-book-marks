8adb6b93f32d56c3303c57312fe0d10dd729b41e
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-02-07 21:14:53 +0100

    fichier edit.blade du dossier links

---
+++ larabookmarks/resources/views/links/edit.blade.php
@@ -0,0 +1,48 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-2xl font-bold text-red-600">{{ isset($link) ? 'Modifier le lien' : 'Ajouter un lien' }}</h2>
+    </x-slot>
+
+    <form action="{{ isset($link) ? route('links.update', $link) : route('links.store') }}" method="POST" class="bg-gray-900 p-6 rounded shadow space-y-4">
+        @csrf
+        @if(isset($link)) @method('PUT') @endif
+
+        <div>
+            <label class="block mb-1">Titre</label>
+            <input type="text" name="title" value="{{ old('title', $link->title ?? '') }}" required
+                class="w-full p-2 rounded border border-red-800 text-black" />
+        </div>
+
+        <div>
+            <label class="block mb-1">URL</label>
+            <input type="url" name="url" value="{{ old('url', $link->url ?? '') }}" required
+                class="w-full p-2 rounded border border-red-800 text-black break-words" />
+        </div>
+
+        <div>
+            <label class="block mb-1">Catégorie</label>
+            <select name="category_id" required class="w-full p-2 rounded border border-red-800 text-black">
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}"
+                        {{ isset($link) && $link->category_id == $category->id ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+      <div>
+    <label class="block mb-1">Tags (séparés par des virgules)</label>
+    <input type="text" name="tags_text" 
+           value="{{ isset($link) ? implode(',', $link->tags->pluck('name')->toArray()) : '' }}"
+           placeholder="Ex: Laravel, PHP, JS"
+           class="w-full p-2 rounded border border-red-800 text-black" />
+</div>
+
+
+
+        <button type="submit" class="bg-red-800 px-4 py-2 rounded hover:bg-red-600">
+            {{ isset($link) ? 'Modifier le lien' : 'Ajouter le lien' }}
+        </button>
+    </form>
+</x-app-layout>
